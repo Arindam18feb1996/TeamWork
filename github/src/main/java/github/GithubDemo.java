59af402318cf33acf0fa001778c6b5332f51b53a
aribhowm <aribhowm@DIN73003068.corp.capgemini.com>
2018-11-23 14:31:48 +0530

    First Commit

---
+++ github/src/main/java/github/GithubDemo.java
@@ -0,0 +1,14 @@
+package github;
+
+public class GithubDemo {
+	
+	void display() {
+		System.out.println("This is beginning");
+	}
+
+	public static void main(String[] args) {
+		GithubDemo demo=new GithubDemo();
+		demo.display();
+	}
+
+}
